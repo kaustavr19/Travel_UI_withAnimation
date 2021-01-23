9b7f3a7bd7acb1a3adb791add90e9d3568f9cc85
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-01-24 00:11:18 +0530

    animation and cards added (small changes still coming)

--- lib/travel/travel_page.dart
+++ lib/travel/travel_page.dart
@@ -10,16 +10,36 @@ class _TravelPageState extends State<TravelPage> with TickerProviderStateMixin {
   final imgurl =
       "https://avatars.githubusercontent.com/u/54073521?s=460&u=435d63cc8d05543a9864c2d5948901294ba522b9&v=4";
   TabController _tabController;
+  double anim = 1.0;
+  double anim2 = 1.0;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _tabController = TabController(vsync: this, length: 4);
+
+    withAnimation(
+        vsync: this,
+        tween: Tween(begin: 1.0, end: 0.0),
+        callBack: (animationVal, controllerVal) {
+          anim = animationVal;
+          setState(() {});
+        });
+
+    withRepeatAnimation(
+        vsync: this,
+        isRepeatReversed: true,
+        tween: Tween(begin: 2.0, end: 3.0),
+        callBack: (animationVal, controllerVal) {
+          anim2 = animationVal;
+          setState(() {});
+        });
   }
 
   @override
   Widget build(BuildContext context) {
+    final tabIndex = _tabController.index;
     return Scaffold(
       backgroundColor: Vx.green600,
       appBar: AppBar(
@@ -51,7 +71,8 @@ class _TravelPageState extends State<TravelPage> with TickerProviderStateMixin {
               .white
               .xl2
               .make()
-              .p8(),
+              .p8()
+              .offset(offset: Offset(-300 * anim, 0)),
           "Exploring the world".text.white.make(),
           VxTextField(
             borderType: VxTextFieldBorderType.none,
@@ -91,25 +112,83 @@ class _TravelPageState extends State<TravelPage> with TickerProviderStateMixin {
               tabs: [
                 Icon(
                   Icons.map,
-                  size: 30,
-                ),
+                  size: 10,
+                ).scale(scaleValue: tabIndex == 0 ? anim2 : 3.0),
                 Icon(
                   Icons.pin_drop,
-                  size: 30,
-                ),
+                  size: 10,
+                ).scale(scaleValue: tabIndex == 1 ? anim2 : 3.0),
                 Icon(
                   Icons.restaurant,
-                  size: 30,
-                ),
+                  size: 10,
+                ).scale(scaleValue: tabIndex == 2 ? anim2 : 3.0),
                 Icon(
                   Icons.person,
-                  size: 30,
-                )
+                  size: 10,
+                ).scale(scaleValue: tabIndex == 3 ? anim2 : 3.0)
               ],
-            )
+            ).h(context.percentHeight * 8),
+            TabBarView(
+              controller: _tabController,
+              children: ["1", "2", "3", "4"]
+                  .map((e) => VStack([
+                        "Discover places in Kolkata"
+                            .text
+                            .gray600
+                            .xl2
+                            .bold
+                            .make(),
+                        20.heightBox,
+                        TravelCard(
+                          imageUrl: imgurl,
+                          title: "Howrah Bridge",
+                          subtitle: "Howrah",
+                        ),
+                        20.heightBox,
+                        TravelCard(
+                          imageUrl: imgurl,
+                          title: "Howrah Bridge",
+                          subtitle: "Howrah",
+                        ),
+                      ]))
+                  .toList(),
+            ).expand()
           ])).white.make(),
         ).expand()
       ]),
     );
+  }
+}
+
+class TravelCard extends StatelessWidget {
+  final String title, subtitle, imageUrl;
+
+  const TravelCard({Key key, this.title, this.subtitle, this.imageUrl})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return HStack([
+      Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ).wh(context.percentWidth * 35, 100).cornerRadius(20),
+      20.widthBox,
+      VStack(
+        [
+          title.text.semiBold.make(),
+          3.heightBox,
+          subtitle.text.make().shimmer(),
+          5.heightBox,
+          [
+            VxRating(
+              onRatingUpdate: (value) {},
+            ),
+            5.widthBox,
+            "(100)".text.xs.gray600.make(),
+          ].row()
+        ],
+        crossAlignment: CrossAxisAlignment.start,
+      ).expand()
+    ]).cornerRadius(8).backgroundColor(Vx.gray200);
   }
 }

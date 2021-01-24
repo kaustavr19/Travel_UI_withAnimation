ec2cfb25729ea04e93c36135238b9801aa43defd
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-01-24 14:25:49 +0530

    christmas tree with gesture movement added

--- lib/colors.dart
+++ lib/colors.dart
@@ -7,4 +7,8 @@ class Coolors {
   static Color purplish = Color(0xffa55591);
   static Color pinkish = Color(0xffca99b9);
   static Color violetish = Color(0xff46384b);
+  static Color yellow1 = Color(0xffc09857);
+  static Color yellow2 = Color(0xff866919);
+  static Color green1 = Color(0xff3e9536);
+  static Color green2 = Color(0xff2b5727);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:travel_app_ui/travel/travel_page.dart';
 import 'colors.dart';
+import 'fun_ui/christmas.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TravelPage(),
+      home: ChristmasTree(),
       theme: ThemeData(
         accentColor: Coolors.reddish,
         primaryColor: Coolors.purplish,
